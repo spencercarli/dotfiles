93826bc3295b5af57da948fd253671d09a45ef22
Spencer Carli <spencercarli@gmail.com>
2018-12-05 09:15:54 -0600

    Update hyper plugins

--- .hyper.js
+++ .hyper.js
@@ -100,7 +100,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-night-owl", "hyper-final-say"],
+  plugins: ["hyper-search", "hyper-night-owl", "hyper-final-say"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
